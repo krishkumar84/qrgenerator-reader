626ab4318a5e857413e863a09505966b32b3be76
krish <114145459+krishkumar84@users.noreply.github.com>
2023-04-10 21:31:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// const sections = gsap.utils.toArray(".panel"),
+//   container = document.querySelector(".container");
+
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".container",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     end: () => "+=" + container.offsetWidth,
+//   },
+// });
 gsap.registerPlugin(ScrollTrigger);
 
 const sections = gsap.utils.toArray(".panel"),
   container = document.querySelector(".container");
+
+const updateScrollTrigger = () => {
+  if (window.innerWidth < 768 || document.activeElement.getElementsByClassName == "qr-text") {
+    // Disable ScrollTrigger animation when the keyboard is open or the viewport width is less than 768px
+    ScrollTrigger.getAll().forEach((trigger) => {
+      trigger.disable();
+    });
+  } else {
+    // Enable ScrollTrigger animation when the keyboard is closed and the viewport width is greater than or equal to 768px
+    ScrollTrigger.getAll().forEach((trigger) => {
+      trigger.enable();
+    });
+  }
+};
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
@@ -14,3 +44,7 @@ gsap.to(sections, {
     end: () => "+=" + container.offsetWidth,
   },
 });
+
+// Detect when the keyboard is open or closed
+window.addEventListener("resize", updateScrollTrigger);
+window.addEventListener("focusout", updateScrollTrigger);
